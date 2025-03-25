9152bd831e7415dae56bf9a4ca4acfa3c90bebda
Grant Cox <gcoxexcel@gmail.com>
2025-03-25 09:55:33 -0400

    Add env variable init for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    AUTH_USERNAME: process.env.AUTH_USERNAME,
+    AUTH_PASSWORD: process.env.AUTH_PASSWORD,
+  },
 };
 
 export default nextConfig;
